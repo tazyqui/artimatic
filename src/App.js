409fdcdc79048026456488ecf1b7ede3b65acc77
Ian Baldwin <mindofianb@gmail.com>
2023-02-01 14:57:50 -0500

    Added react-p5 library and implemented sketch in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AsemicScript from './sketches/asemic_script';
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rea
         </a>
+        <AsemicScript />
       </header>
     </div>
   );
